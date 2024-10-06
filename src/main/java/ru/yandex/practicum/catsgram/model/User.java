7e3bb9323a66c24d29bfebe8b5aca9da5b147495
Artem Oleshev <sunlightpattern@yandex.ru>
2024-10-06 20:28:42 +0300

    refactor: rename registration field

--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -14,5 +14,5 @@ public class User {
     private String username;
     private String email;
     private String password;
-    private Instant registration;
+    private Instant registrationDate;
 }
